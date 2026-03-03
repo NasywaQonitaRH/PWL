8c1f4fa8077392e169fb00e6f3751c2847ad38fc
244107020170-png <244107020170@student.polinema.ac.id>
2026-03-03 21:06:11 +0700

    DB Facade

--- POS/app/Http/Controllers/LevelController.php
+++ POS/app/Http/Controllers/LevelController.php
@@ -8,7 +8,16 @@ use Illuminate\Support\Facades\DB;
 class LevelController extends Controller
 {
     public function index() {
-        DB::insert('insert into m_level(level_code,  level_nama, created_at,) values(?, ?, ?)', ['CUS', 'Pelanggan', now(), now()]);
-        return 'Insert data baru berhasil';
+        // DB::insert('insert into m_level(level_kode, level_nama, created_at, updated_at) values(?, ?, ?, ?)', ['CUS', 'Pelanggan', now(), now()]);
+        // return 'Insert data baru berhasil';
+
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'Update data berhasil. Jumlah data yang di update: ' . $row. ' baris';
+
+        // $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        // return 'Delete data berhasil. Jumlah data yang di delete: ' . $row. ' baris';
+        
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
     }
 }
