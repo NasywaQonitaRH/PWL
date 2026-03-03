81aef359f39c524765e15ccaf68d4755e7b8f42c
244107020170-png <244107020170@student.polinema.ac.id>
2026-03-03 22:34:30 +0700

    eloquent orm

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function profile($id,$name) {
-        return view('user', compact('id', 'name'));
+    public function index() {
+        // return view('user', compact('id', 'name'));
+        $data = [
+            // 'username' => 'customer-1',
+            'nama' => 'Pelanggan Pertama',
+            // 'password' => Hash::make('12345'),
+            // 'level_id' => 4
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+        
+        $user = UserModel::all();
+        return view('user', ['data' => $user]); 
     }
 }
--- POS/resources/views/user.blade.php
+++ POS/resources/views/user.blade.php
@@ -1,3 +1,29 @@
-<h1>User Pofile</h1>
+{{-- <h1>User Pofile</h1>
 <p>User ID: {{ $id }}</p>
-<p>User Name: {{ $name }}</p>
+<p>User Name: {{ $name }}</p> --}}
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Data User</title>
+    </head>
+    <body>
+        <h1>Data User</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+            </tr>
+            @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{$d ->level_id}}</td>
+            </tr>
+            @endforeach
+        </table>
+    </body>
+</html>
