739fc2fb1ae82103f209995ed5e61659caaac150
244107020170-png <244107020170@student.polinema.ac.id>
2026-03-10 22:16:30 +0700

    crud

--- POS/app/Http/Controllers/UserController.php
+++ POS/app/Http/Controllers/UserController.php
@@ -9,24 +9,65 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }    
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'level_id' => $request->level_id,
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make($request->password)
         ]);
+        return redirect('/user');
+    }
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+    public function ubah_simpan(Request $request, $id)
+    {
+        $user = UserModel::find($id);
 
-        $user->username = 'manager12';
+        $user->level_id = $request->level_id;
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        
         $user->save();
 
-        $user->wasChanged();
-        $user->wasChanged('username');
-        $user->wasChanged(['username', 'level_id']);
-        $user->wasChanged('nama');
-        dd($user->wasChanged(['nama', 'username']));
-    }    
-}
+        return redirect('/user');
+    }
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+        
+        return redirect('/user');
+    }
 
+}
+// $user = UserModel::create([
+        //     'username' => 'manager11',
+        //     'nama' => 'Manager11',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+
+        // $user->username = 'manager12';
+        // $user->save();
+
+        // $user->wasChanged();
+        // $user->wasChanged('username');
+        // $user->wasChanged(['username', 'level_id']);
+        // $user->wasChanged('nama');
+        // dd($user->wasChanged(['nama', 'username']));
 // $user->username = 'manager-55';
 
         // $user ->isDirty();
--- POS/resources/views/user.blade.php
+++ POS/resources/views/user.blade.php
@@ -3,7 +3,33 @@
 <p>User Name: {{ $name }}</p> --}}
 
 <!DOCTYPE html>
-<html>
+    <body>
+        <h1>Data User</h1>
+        <a href="/PWL/POS/public/user/tambah">+ Tambah User</a>
+        <table border="1" cellpading="2" cellspacing="0">
+            <tr>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>ID Level Pengguna</td>
+                <td>Aksi</td>
+            </tr>
+            @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td>
+                    <a href="/PWL/POS/public/user/ubah/{{ $d->user_id }}">Ubah</a> |
+                    <a href="/PWL/POS/public/user/hapus/{{ $d->user_id }}">Hapus</a>
+                </td>
+            </tr>
+            @endforeach
+    </body>
+</html>
+
+{{-- <html>
     <head>
         <title>Data User</title>
     </head>
@@ -16,16 +42,15 @@
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
                 {{-- <th>Jumlah Pengguna</th> --}}
-            </tr>
+            {{-- </tr>
             {{-- @foreach ($data as $user) --}}
-            <tr>
+            {{-- <tr>
                 <td>{{ $data->user_id }}</td>
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
                 <td>{{ $data->level_id }}</td>
                 {{-- <td>{{$count}}</td> --}}
-            </tr>
+            {{-- </tr> --}}
             {{-- @endforeach --}}
-        </table>
-    </body>
-</html>
+        {{-- </table> --}}
+    {{-- </body>  --}} 
